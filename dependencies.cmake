b5536b81db80e8d196d7b130112dae29f48e4c08
German Espinosa <germanespinosa@gmail.com>
2021-12-07 21:39:30 -0600

    Update dependencies.cmake

--- dependencies.cmake
+++ dependencies.cmake
@@ -42,11 +42,6 @@ function(install_dependency git_repo)
     execute_process(COMMAND bash -c "cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} '-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}' -G 'CodeBlocks - Unix Makefiles' ${dependency_folder}"
             WORKING_DIRECTORY ${destination_folder})
 
-
-    execute_process(COMMAND bash -c "if [ -d dependable ]; then echo exists; else echo doesnt; fi"
-            WORKING_DIRECTORY ${destination_folder}
-            RESULT_VARIABLE  include_folder_exists)
-
     execute_process(COMMAND bash -c "[ -d dependency_include ]"
             WORKING_DIRECTORY ${destination_folder}
             RESULT_VARIABLE  include_folder_exists)
