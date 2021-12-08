ffe34ff4c07e6be4278e78df0a6978ffd17d83bd
German Espinosa <germanespinosa@gmail.com>
2021-12-08 11:42:34 -0600

    Update dependencies.cmake

--- dependencies.cmake
+++ dependencies.cmake
@@ -47,7 +47,7 @@ function(install_dependency git_repo)
     execute_process(COMMAND mkdir ${repo_name} -p
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} )
 
-    execute_process(COMMAND bash -c "cmake --no-warn-unused-cli -DCATCH_TEST=NO_TEST -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} '-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}' -G 'CodeBlocks - Unix Makefiles' ${dependency_folder}"
+    execute_process(COMMAND bash -c "CATCH_TEST=NO_TEST cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} '-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}' -G 'CodeBlocks - Unix Makefiles' ${dependency_folder}"
             WORKING_DIRECTORY ${destination_folder})
 
     execute_process(COMMAND bash -c "[ -d dependency_include ]"
