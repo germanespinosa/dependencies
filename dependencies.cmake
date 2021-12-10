bd85e532c0f7d2fcac9f9710ae8d2af3df804d9f
German Espinosa <germanespinosa@gmail.com>
2021-12-10 10:29:25 -0600

    Update dependencies.cmake

--- dependencies.cmake
+++ dependencies.cmake
@@ -3,6 +3,10 @@ include_directories(${CMAKE_CURRENT_BINARY_DIR}/dependency_include)
 file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/dependencies_outputs.txt)
 file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/dependencies_packages.txt)
 
+set(dependencies_folder "${CMAKE_CURRENT_SOURCE_DIR}/dependencies" CACHE PATH "")
+execute_process(COMMAND mkdir ${dependencies_folder} -p)
+
+
 macro (copy_include)
     foreach(include_folder ${ARGN})
         execute_process(COMMAND bash -c "cp ${include_folder}/* ${CMAKE_CURRENT_BINARY_DIR}/dependency_include/ -r"
@@ -41,51 +45,34 @@ macro (add_dependency_output_directory dependency_output_directory)
 endmacro()
 
 macro(install_dependency git_repo)
-    
     execute_process(COMMAND basename ${git_repo}
             OUTPUT_VARIABLE repo_name )
-
     string(REPLACE "\n" "" repo_name ${repo_name})
-
     message(STATUS "\nConfiguring dependency ${repo_name}")
-
-    set(dependencies_folder "${CMAKE_CURRENT_SOURCE_DIR}/dependencies")
-    
-    execute_process(COMMAND mkdir ${dependencies_folder} -p)
-
     set(dependency_folder "${dependencies_folder}/${repo_name}")
-
     execute_process(COMMAND bash -c "[ -d ${repo_name} ]"
             WORKING_DIRECTORY ${dependencies_folder}
             RESULT_VARIABLE  folder_exists)
-
     if (${folder_exists} EQUAL 0)
         execute_process(COMMAND git pull
                 WORKING_DIRECTORY ${dependency_folder})
     else()
         execute_process(COMMAND git -C ${dependencies_folder} clone ${git_repo})
     endif()
-
     set(destination_folder ${CMAKE_CURRENT_BINARY_DIR}/${repo_name})
-
     execute_process(COMMAND mkdir ${repo_name} -p
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} )
-
     execute_process(COMMAND bash -c "BUILD_AS_DEPENDENCY=TRUE CATCH_TESTS=NO_TESTS cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} '-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}' -G 'CodeBlocks - Unix Makefiles' ${dependency_folder}"
             WORKING_DIRECTORY ${destination_folder})
-
     execute_process(COMMAND bash -c "[ -d dependency_include ]"
             WORKING_DIRECTORY ${destination_folder}
             RESULT_VARIABLE  include_folder_exists)
-
     if (${include_folder_exists} EQUAL 0)
         copy_include(${destination_folder}/dependency_include)
     endif()
-
     execute_process(COMMAND bash -c "[ -f dependencies_outputs.txt ]"
             WORKING_DIRECTORY ${destination_folder}
             RESULT_VARIABLE  dependencies_outputs_exists)
-
     if (${dependencies_outputs_exists} EQUAL 0)
         message(STATUS "dependency_outputs found!")
         file(READ ${destination_folder}/dependencies_outputs.txt dependencies_outputs)
@@ -95,11 +82,9 @@ macro(install_dependency git_repo)
             endif()
         endforeach()
     endif()
-
     execute_process(COMMAND bash -c "[ -f dependencies_packages.txt ]"
             WORKING_DIRECTORY ${destination_folder}
             RESULT_VARIABLE  dependencies_packages_exists)
-
     if (${dependencies_packages_exists} EQUAL 0)
         message(STATUS "dependencies_packages found!")
         file(READ ${destination_folder}/dependencies_packages.txt dependencies_packages)
@@ -109,20 +94,14 @@ macro(install_dependency git_repo)
             endif()
         endforeach()
     endif()
-
     execute_process(COMMAND make -j
             WORKING_DIRECTORY ${destination_folder})
-
     set (repo_targets "${destination_folder}/${repo_name}Targets.cmake")
-
     set (variadic_args ${ARGN})
     list(LENGTH variadic_args variadic_count)
     if (${variadic_count} GREATER 0)
         list(GET variadic_args 0 package_name)
         add_dependency_package ("${package_name}|${destination_folder}")
     endif ()
-    
-    
     add_dependency_output_directory(${destination_folder})
-   
 endmacro()
