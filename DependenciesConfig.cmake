4fa06e5ca7d985f257c7bfc533e2d23dd81c5244
German Espinosa <germanespinosa@gmail.com>
2022-09-26 17:41:45 -0500

    new-version

--- DependenciesConfig.cmake
+++ DependenciesConfig.cmake
@@ -17,8 +17,8 @@ endif()
 set(DEPENDENCIES_DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/dependencies-build")
 
 make_directory(${DEPENDENCIES_DESTINATION})
-
-set(DEPENDENCY_CMAKE "cmake '-DBUILD_AS_DEPENDENCY=TRUE' '-DDEPENDENCIES_FOLDER=${DEPENDENCIES_FOLDER}' --no-warn-unused-cli -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} '-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}' -Wno-dev -DCATCH_TESTS=DISABLED ${DEPENDENCIES_FOLDER}")
+#${CMAKE_COMMAND}
+set(DEPENDENCY_CMAKE "${CMAKE_COMMAND} '-DBUILD_AS_DEPENDENCY=TRUE' '-DDEPENDENCIES_FOLDER=${DEPENDENCIES_FOLDER}' --no-warn-unused-cli -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} '-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}' -Wno-dev -DCATCH_TESTS=DISABLED ${DEPENDENCIES_FOLDER}")
 
 include_directories(${DEPENDENCIES_FOLDER})
 
@@ -77,6 +77,167 @@ macro (add_dependency_output_directory dependency_output_directory)
     message(STATUS "Adding folder ${dependency_output_directory} to dependency tree")
     append_new("${CMAKE_CURRENT_BINARY_DIR}/dependencies_outputs.txt" "${dependency_output_directory};")
     link_directories(${dependency_output_directory})
+endmacro()
+
+macro (get_git_dependency_folder DEPENDENCY_REPOSITORY DEPENDENCY_FOLDER_NAME)
+    cmake_parse_arguments(DEPENDENCY "" "BRANCH;TAG" "" ${ARGN} )
+    get_filename_component(DEPENDENCY_REPOSITORY_NAME ${DEPENDENCY_REPOSITORY} NAME)
+
+    if(NOT "${DEPENDENCY_BRANCH}${DEPENDENCY_TAG}" STREQUAL "")
+        STRING(REGEX REPLACE "[^a-zA-Z0-9]" "_" DEPENDENCY_BRANCH_FOLDER "${DEPENDENCY_BRANCH}")
+        STRING(REGEX REPLACE "[^a-zA-Z0-9]" "_" DEPENDENCY_TAG_FOLDER "${DEPENDENCY_TAG}")
+        set(${DEPENDENCY_FOLDER_NAME} "${DEPENDENCY_REPOSITORY_NAME}_${DEPENDENCY_BRANCH_FOLDER}${DEPENDENCY_TAG_FOLDER}")
+    else()
+        set(${DEPENDENCY_FOLDER_NAME} "${DEPENDENCY_REPOSITORY_NAME}")
+    endif()
+endmacro()
+
+macro (update_git_dependency DEPENDENCY_REPOSITORY DEPENDENCY_FOLDER_NAME DEPENDENCY_HAS_UPDATES)
+    cmake_parse_arguments(DEPENDENCY "AUTO_UPDATE;CONNECTED;VERBOSE" "BRANCH;TAG" "" ${ARGN} )
+
+    if("${DEPENDENCY_BRANCH}" STREQUAL "")
+        set(DEPENDENCY_HAS_BRANCH FALSE)
+    else()
+        set(DEPENDENCY_HAS_BRANCH TRUE)
+    endif()
+
+    if("${DEPENDENCY_TAG}" STREQUAL "")
+        set(DEPENDENCY_HAS_TAG FALSE)
+    else()
+        set(DEPENDENCY_HAS_TAG TRUE)
+    endif()
+
+    if (${DEPENDENCY_HAS_TAG} AND ${DEPENDENCY_HAS_BRANCH})
+        message(FATAL_ERROR "BRANCH and TAG cannot be uses simultaneously" )
+    endif()
+
+    set(${DEPENDENCY_HAS_UPDATES} FALSE)
+
+    get_git_dependency_folder(${DEPENDENCY_REPOSITORY} UPDATE_DEPENDENCY_FOLDER_NAME ${ARGN})
+
+    set(${DEPENDENCY_FOLDER_NAME} ${UPDATE_DEPENDENCY_FOLDER_NAME})
+
+    set(DEPENDENCY_FOLDER "${DEPENDENCIES_FOLDER}/${UPDATE_DEPENDENCY_FOLDER_NAME}")
+
+    message("DEPENDENCY_FOLDER: ${DEPENDENCY_FOLDER}")
+
+    #locks the dependency for concurrent cmakes running
+    set(DEPENDENCY_LOCK "${DEPENDENCIES_FOLDER}/${DEPENDENCY_REPOSITORY_NAME}.lock")
+    file(LOCK ${DEPENDENCY_LOCK})
+
+    # if folder doesn't exists does the cloning
+    if (NOT EXISTS "${DEPENDENCY_FOLDER}")
+        if (${DEPENDENCY_HAS_BRANCH} OR ${DEPENDENCY_HAS_TAG})
+            if (${DEPENDENCY_VERBOSE})
+                message("##############################################################################################")
+                message("CLONING: ${DEPENDENCY_REPOSITORY} BRANCH ${DEPENDENCY_BRANCH}${DEPENDENCY_TAG} INTO ${DEPENDENCY_FOLDER}")
+                message("##############################################################################################")
+            endif()
+            execute_process(COMMAND git clone --branch "${DEPENDENCY_BRANCH}${DEPENDENCY_TAG}" ${DEPENDENCY_REPOSITORY} ${DEPENDENCY_FOLDER}
+                    WORKING_DIRECTORY ${DEPENDENCIES_FOLDER})
+        else()
+            if (${DEPENDENCY_VERBOSE})
+                message("##############################################################################################")
+                message("CLONING: ${DEPENDENCY_REPOSITORY} INTO ${DEPENDENCY_FOLDER}")
+                message("##############################################################################################")
+            endif()
+            execute_process(COMMAND git clone ${DEPENDENCY_REPOSITORY} ${DEPENDENCY_FOLDER}
+                    WORKING_DIRECTORY ${DEPENDENCIES_FOLDER})
+        endif()
+        set(${DEPENDENCY_HAS_UPDATES} TRUE)
+        #if it is a disconnected dependency, remove git link
+        if (NOT ${DEPENDENCY_CONNECTED})
+            if (${DEPENDENCY_VERBOSE})
+                message("##############################################################################################")
+                message("REMOVING REPOSITORY LINK")
+                message("##############################################################################################")
+            endif()
+            file(REMOVE_RECURSE ${DEPENDENCY_FOLDER}/.git)
+            file(REMOVE ${DEPENDENCY_FOLDER}/.gitmodules)
+        endif()
+    endif()
+
+    #if it is a connected dependency and auto-update is set, check for changes
+    if (${DEPENDENCY_CONNECTED})
+        if (${DEPENDENCY_AUTO_UPDATE})
+            if (${DEPENDENCY_VERBOSE})
+                message("##############################################################################################")
+                message("PULLING CHANGES")
+                message("##############################################################################################")
+            endif()
+            execute_process(COMMAND git pull
+                    WORKING_DIRECTORY ${DEPENDENCY_FOLDER}
+                    OUTPUT_VARIABLE DEPENDENCY_PULL_OUTPUT
+                    ERROR_QUIET)
+
+            if ( NOT "${DEPENDENCY_PULL_OUTPUT}" MATCHES "Already up to date.")
+                if (${DEPENDENCY_VERBOSE})
+                    message("##############################################################################################")
+                    message("GIT PULL RESULT: UPDATES FOUND")
+                    message("##############################################################################################")
+                endif()
+                set(${DEPENDENCY_HAS_UPDATES} TRUE)
+            endif()
+        endif()
+    endif()
+    file(LOCK ${DEPENDENCY_LOCK} RELEASE)
+    file(REMOVE ${DEPENDENCY_LOCK})
+endmacro()
+
+macro (install_git_dependency DEPENDENCY_REPOSITORY)
+    cmake_parse_arguments(DEPENDENCY
+            "AUTO_UPDATE;CONNECTED;NO_BUILD;VERBOSE;ADD_SUBDIRECTORY;DOWNLOAD_ONLY;CMAKE_PROJECT"
+            "BRANCH;TAG;TARGET"
+            "PACKAGES;CMAKE_OPTIONS;IMPORT_TARGETS;INCLUDE_DIRECTORIES"
+            ${ARGN} )
+
+    update_git_dependency(${DEPENDENCY_REPOSITORY} DEPENDENCY_FOLDER_NAME DEPENDENCY_HAS_UPDATES ${ARGN} )
+
+    set(DEPENDENCY_FOLDER "${DEPENDENCIES_FOLDER}/${DEPENDENCY_FOLDER_NAME}" )
+    set(DEPENDENCY_DESTINATION "${DEPENDENCIES_DESTINATION}/${DEPENDENCY_FOLDER_NAME}")
+
+    if (${DEPENDENCY_ADD_SUBDIRECTORY})
+        make_directory("${DEPENDENCY_DESTINATION}")
+        add_subdirectory(${DEPENDENCY_FOLDER} ${DEPENDENCY_DESTINATION})
+    elseif(${DEPENDENCY_CMAKE_PROJECT})
+        make_directory("${DEPENDENCY_DESTINATION}")
+        message("DEPENDENCY_DESTINATION: ${DEPENDENCY_DESTINATION}")
+        set(DEPENDENCY_CMAKE_COMMAND "${DEPENDENCY_CMAKE} '-DDEPENDENCY=${DEPENDENCY_FOLDER_NAME}'")
+
+        execute_process(COMMAND bash -c "${DEPENDENCY_CMAKE} '-DDEPENDENCY=${DEPENDENCY_FOLDER_NAME}'"
+                WORKING_DIRECTORY ${DEPENDENCY_DESTINATION}
+                RESULT_VARIABLE DEPENDENCY_CMAKE_RESULT )
+
+
+        message("DEPENDENCY_CMAKE_RESULT: ${DEPENDENCY_CMAKE_RESULT}")
+        if (NOT ${DEPENDENCY_CMAKE_RESULT} EQUAL "0")
+            message(FATAL_ERROR "failed to load dependency cmake file" )
+        endif()
+        if (NOT ${NO_BUILD})
+            execute_process(COMMAND make -j
+                    WORKING_DIRECTORY ${DEPENDENCY_DESTINATION}
+                    RESULT_VARIABLE DEPENDENCY_MAKE_RESULT)
+            if (NOT ${DEPENDENCY_MAKE_RESULT} EQUAL "0")
+                message(FATAL_ERROR "failed to build dependency cmake file" )
+            endif()
+        endif()
+    endif()
+
+    if("${DEPENDENCY_INCLUDE_DIRECTORIES}" STREQUAL "")
+        set(DEPENDENCY_HAS_INCLUDE_DIRECTORIES FALSE)
+    else()
+        set(DEPENDENCY_HAS_INCLUDE_DIRECTORIES TRUE)
+    endif()
+
+    if(${DEPENDENCY_HAS_INCLUDE_DIRECTORIES})
+        foreach(DEPENDENCY_INCLUDE_DIRECTORY ${DEPENDENCY_INCLUDE_DIRECTORIES})
+            if("${DEPENDENCY_TARGET}" STREQUAL "")
+                include_directories(${DEPENDENCY_FOLDER}/${DEPENDENCY_INCLUDE_DIRECTORY})
+            else()
+                target_include_directories(${DEPENDENCY_TARGET} ${DEPENDENCY_FOLDER}/${DEPENDENCY_TARGET_INCLUDE_DIRECTORY})
+            endif()
+        endforeach()
+    endif()
 endmacro()
 
 macro(install_dependency DEPENDENCY_REPOSITORY)
